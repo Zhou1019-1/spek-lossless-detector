f2e22c574a9e123a47f4da92b4a034ab2fe20bf0
Zhou1019-1 <430768125@qq.com>
2026-06-05 11:52:06 +0800

    fix : use HashRouter for GitHub Pages static hosting

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'  // ← 改成这行
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>  // ← 改成这行
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
